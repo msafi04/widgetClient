3d384e4bced5601f620e47d1aa8b26bdd474344d
mohamedsafi <mohamed.safi@gmail.com>
2023-12-17 21:46:07 +0800

    custom styles

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -17,54 +17,110 @@ const Popup = ({ closePopup, setClosePopup }) => {
     setFormInput("");
     setClosePopup(true);
   };
+
+  const containerStyle = {
+    backgroundColor: "#e5d0d0",
+  };
+
+  const minContainerStyle = {
+    backgroundColor: "#e5d0d0",
+  };
+
+  const closeBtnStyle = {
+    fontSize: "18px",
+    color: "#4f4e4e",
+  };
+
+  const contentStyle = {
+    backgroundColor: "#e5d0d0",
+  };
+  const headerStyle = {
+    fontSize: "24px",
+    fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+    color: "black",
+  };
+  const clickNextStyle = {
+    fontSize: "25px",
+    color: "#5c5b5b",
+  };
+
+  const submitBtnStyle = {
+    fontSize: "15px",
+    color: "#fcfafa",
+    width: "6rem",
+    height: "2rem",
+    backgroundColor: "#414f41",
+    borderRadius: "15px"
+  };
+
   return (
-    <div className={`${minimizePopup ? "minContainer" : "container"}`}>
-      <div className="topBtn">
+    <div
+      className={`${minimizePopup ? "popup-minContainer" : "popup-container"}`}
+      style={minimizePopup ? minContainerStyle : containerStyle}
+      onClick={() => minimizePopup ? setMinimizePopup((prev) => !prev) : null}
+    >
+      <div className="popup-topBtn">
         {!minimizePopup ? (
           <FaMinus
             className="minBtn"
             onClick={() => setMinimizePopup((prev) => !prev)}
           />
-        ) : (
-          <FaWindowMaximize
-            className="maxBtn"
-            onClick={() => setMinimizePopup((prev) => !prev)}
-          />
-        )}
-        <IoClose className="closeBtn" onClick={() => setClosePopup(true)} />
+        ) : null
+        // (
+          // <FaWindowMaximize
+          //   className="maxBtn"
+          //   onClick={() => setMinimizePopup((prev) => !prev)}
+          // />
+        //)
+        }
+        <IoClose
+          className="popup-closeBtn"
+          onClick={() => setClosePopup(true)}
+          style={closeBtnStyle}
+        />
       </div>
       {!minimizePopup && !clickNext && (
-        <div className="content">
-          <p className="header">How do you like the service?</p>
-          <div className="feedback">
+        <div className="popup-content" style={contentStyle}>
+          <p className="popup-header" style={headerStyle}>
+            How do you like our service?
+          </p>
+          <div className="popup-feedback">
             <Ratings
               selectedNumber={selectedNumber}
               setSelectedNumber={setSelectedNumber}
             />
           </div>
           <FaCircleArrowRight
-            className="nextStep"
+            className="popup-nextStep"
+            style={clickNextStyle}
             onClick={() => setClickNext(true)}
             disabled={selectedNumber === 0}
           />
         </div>
       )}
       {clickNext && (
-        <div className="nextContent">
-          <p className="header">Tell Us More</p>
-          <div className="feedback">
+        <div className="popup-nextContent">
+          <p className="popup-header" style={headerStyle}>
+            Tell Us More
+          </p>
+          <div className="popup-feedback">
             <textarea
-              className="inputBox"
+              className="popup-inputBox"
               name="textArea"
               form="textForm"
               onChange={(e) => setFormInput(e.target.value)}
             ></textarea>
             <form
               name="textForm"
-              className="textForm"
+              className="popup-textForm"
               onSubmit={handleOnSubmit}
             >
-              <button type="submit" className="submitBtn" disabled={!formInput}>
+              <button
+                type="submit"
+                className="popup-submitBtn"
+                style={submitBtnStyle}
+                disabled={!formInput}
+              >
                 Submit
               </button>
             </form>
